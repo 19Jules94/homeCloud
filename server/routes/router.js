589e05aa9273ce7590c76d243e3553027bf70dbd
19Jules94 <19jules94@gmail.com>
2020-08-30 17:17:48 +0200

    moveFile function()

--- server/routes/router.js
+++ server/routes/router.js
@@ -9,30 +9,41 @@ const storage = process.env.STORAGE;
 
 const processPath = (storagePath) => {
   const relativePath = storagePath ? storagePath.replace(/-/g, "/") : "/";
-  //console.log(path.join(storage,relativePath));
   return { relativePath, absolutePath: path.join(storage, relativePath) };
 };
 
-router.post("/:path?", (req, res) => {
-  let fil;
-  let fullPath;
+const moveFile = (pathDestino, file) => {
+  return new Promise((resolve, reject) => {
+    file.mv(path.join(pathDestino,file.name), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+router.post("/:path?", async(req, res,next) => {
+  let files = req.files.files;  
   const pathDirectorio = processPath(req.params.path);
-  //console.log(pathDirectorio);
-  if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send("No files were uploaded.");
-    return;
+  if(!Array.isArray(files)){
+    files=[files];
   }
-  //console.log("req.files >>>", req.files);
-  fil = req.files.fil;
-  fullPath =pathDirectorio.absolutePath+fil.name;
-  console.log(fullPath);
-  fil.mv(fullPath,function(err){
-    if(err){
-      return res.status(500).send(err);
+
+  try{
+    for (const file of files){
+      await moveFile(pathDirectorio.absolutePath,file);
     }
-    res.send('File uploaded to ' + fullPath);
-  })
+  }catch(err){
+    return next(err);
+  }
+  res.json({
+    message:"Subido con exito",
+    path:pathDirectorio.relativePath
+  });
 });
+
 
 router.get("/:path?", async (req, res) => {
   try {
@@ -40,13 +51,7 @@ router.get("/:path?", async (req, res) => {
     //console.log({ path: directorio });
     const directo = await fs.promises.opendir(directorio.absolutePath);
     const contenido = { archivos: [], directorios: [] };
-
-    /*
-    Los corchetes se usan para series que poseen valores simples, mientras que las llaves 
-    son utilizadas para las series en que hay distintos objetos y 
-    propiedades con diferentes valores
-    */
-
+  
     for await (const dir of directo) {
       if (dir.isDirectory()) {
         contenido.directorios.push(dir.name);
